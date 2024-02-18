8628420d4eb4c53dbd3aae1f1efe95b6aa4386e2
Falk33n <tim.falk.webd23jon@edu.tucsweden.se>
2024-02-18 14:39:16 +0100

    Changed files

---
+++ backendSocial/backendSocial/Model/PostDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backendSocial.Model
+{
+    public class PostDbContext : DbContext
+    {
+        public DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SocialMediaApp;");
+        }
+    }
+}
---
+++ backendSocial/backendSocial/Model/PostService.cs
@@ -0,0 +1,94 @@
+﻿using System.Data.Common;
+
+namespace backendSocial.Model
+{
+    public class PostService
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public void CreatePost(Post post)
+        {
+            if (!ValidationUtils.IsValid(post))
+            {
+                throw new ValidationException("Title, Author, and Content are required fields.");
+            }
+
+            try
+            {
+                _postRepository.CreatePost(post);
+            }
+            catch (DbException ex)
+            {
+                throw new DataAccessException("Failed to create post.", ex);
+            }
+        }
+
+        public List<Post> ReadAllPosts()
+        {
+            try
+            {
+                return _postRepository.ReadAllPosts();
+            }
+            catch (DbException ex)
+            {
+                throw new DataAccessException("Failed to retrieve all posts.", ex);
+            }
+        }
+
+        public Post ReadPostById(int id)
+        {
+            try
+            {
+                var post = _postRepository.ReadPostById(id);
+                if (post == null)
+                {
+                    throw new PostNotFoundException($"Post with ID {id} not found.");
+                }
+                return post;
+            }
+            catch (DbException ex)
+            {
+                throw new DataAccessException("Failed to retrieve post.", ex);
+            }
+        }
+
+        public void UpdatePost(Post post)
+        {
+            if (!ValidationUtils.IsValid(post))
+            {
+                throw new ValidationException("Title, Author, and Content are required fields.");
+            }
+
+            try
+            {
+                _postRepository.UpdatePost(post);
+            }
+            catch (DbException ex)
+            {
+                throw new DataAccessException("Failed to update post.", ex);
+            }
+        }
+
+        public void DeletePost(int id)
+        {
+            try
+            {
+                _postRepository.DeletePost(id);
+            }
+            catch (DbException ex)
+            {
+                throw new DataAccessException("Failed to delete post.", ex);
+            }
+        }
+    }
+
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message) { }
+    }
+}
