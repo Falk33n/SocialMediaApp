5a15377a259beb559bdc3120341a594da8e81bc6
Falk33n <tim.falk.webd23jon@edu.tucsweden.se>
2024-02-19 20:57:22 +0100

    fix

--- backendSocial/Program.cs
+++ backendSocial/Program.cs
@@ -6,32 +6,18 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
-        // Register database context if using Entity Framework
-        // e.g., builder.Services.AddDbContext<MyDbContext>();
-
-        // Register PostService and its dependency
         builder.Services.AddDbContext<BlogPostDbContext>();
         builder.Services.AddTransient<BlogPostService, BlogPostService>();
         builder.Services.AddTransient<IBlogPostRepository, BlogPostRepository>();
 
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
         app.UseHttpsRedirection();
         app.UseAuthorization();
-
         app.MapControllers();
-
         app.Run();
     }
 }
