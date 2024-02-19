6738dd53324474d618889959c6bacf26f3831a63
Falk33n <tim.falk.webd23jon@edu.tucsweden.se>
2024-02-19 14:13:17 +0100

    fix

---
+++ backendSocial/Model/IBlogPostRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace backendSocial.Model
+{
+    public interface IBlogPostRepository
+    {
+        void CreatePost(BlogPost post);
+        List<BlogPost> ReadAllPosts();
+        BlogPost ReadPostById(int id);
+        void UpdatePost(BlogPost post);
+        void DeletePost(int id);
+    }
+}
---
+++ backendSocial/Model/ValidationUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace backendSocial.Model
+{
+    public static class ValidationUtils
+    {
+        public static bool IsValid(BlogPost blogPost)
+        {
+            return !string.IsNullOrEmpty(blogPost.Title) && !string.IsNullOrEmpty(blogPost.Author) && !string.IsNullOrEmpty(blogPost.Content);
+        }
+    }
+}
